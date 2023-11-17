191b544763b8174417b4165ec7d851ba51ef1db2
Wyatt <wyattlefevre@gmail.com>
2023-11-16 21:50:47 -0700

    fix initial data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Tree } from '@/components/Tree'
 import initialTree from '../../data/current_tree.json'
 import { SkillTree } from '@/types'
 export default function Home() {
+  // TODO: use zod to ensure clean initial data
   return (
     <main>
       <Tree tree={initialTree as SkillTree} />
